5ddbeb202f0a59d40892433b1d13bcac1b17bd99
E342 <00161220@uca.edu.sv>
2025-05-04 23:36:12 -0600

    Updating layer Entities

--- layers-backend/entities/dto/user.go
+++ layers-backend/entities/dto/user.go
@@ -1,11 +1,20 @@
 package dto
 
+type MetadataDTO struct {
+	CreatedAt string `json:"created_at,omitempty"`
+	CreatedBy string `json:"created_by,omitempty"`
+	UpdatedAt string `json:"updated_at,omitempty"`
+	UpdatedBy string `json:"updated_by,omitempty"`
+}
+
 type CreateUser struct {
-	Name  string
-	Email string
+	Name     string      `json:"name"`
+	Email    string      `json:"email"`
+	Metadata MetadataDTO `json:"metadata"`
 }
 
 type UpdateUser struct {
-	Name  string
-	Email string
+	Name     string      `json:"name"`
+	Email    string      `json:"email"`
+	Metadata MetadataDTO `json:"metadata"`
 }
