58c2b2fbc841c8351c04b347cadbcd8d7f16bbca
E342 <00161220@uca.edu.sv>
2025-05-04 23:39:35 -0600

    Updating Main

--- layers-backend/main.go
+++ layers-backend/main.go
@@ -6,6 +6,7 @@ import (
 	"layersapi/controllers"
 	"layersapi/repositories/files/csv"
 	"layersapi/services"
+	"log"
 	"net/http"
 )
 
@@ -16,9 +17,14 @@ func main() {
 	userService := services.NewUserService(userRepository)
 	userController := controllers.NewUserController(*userService)
 
+	r.HandleFunc("/users/{id}", userController.GetUserByIDHandler).Methods(http.MethodGet)
+	r.HandleFunc("/users", userController.CreateUserHandler).Methods(http.MethodPost)
+	r.HandleFunc("/users/{id}", userController.UpdateUserHandler).Methods(http.MethodPut)
+	r.HandleFunc("/users/{id}", userController.DeleteUserHandler).Methods(http.MethodDelete)
 	r.HandleFunc("/users", userController.GetAllUsersHandler).Methods(http.MethodGet)
 
 	fmt.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", r)
-
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("Server error: ", err)
+	}
 }
